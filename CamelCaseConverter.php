5923e1d59f099074c6617516c7cf30b21c6219a5
Rod Mcnew <rmcnew@relivinc.com>
2013-06-11 09:46:02 -0500

    add files

---
+++ CamelCaseConverter.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @author    Rod Mcnew
+ * @license   License.txt New BSD License
+ */
+class CamelCaseConverter {
+    /*
+     * Converts camelCase to lower-case-hyphens
+     *
+     * @param string $value the value to convert
+     *
+     * @return string
+     */
+    function camelToHyphens($value)
+    {
+        return strtolower(preg_replace('/([a-zA-Z])(?=[A-Z])/', '$1-', $value));
+    }
+
+    /*
+     * Converts lower-case-hyphens to camelCase
+     *
+     * @param string $value the value to convert
+     *
+     * @return string
+     */
+    function hyphensToCamel($value){
+        return preg_replace("/\-(.)/e", "strtoupper('\\1')", $value);
+    }
+}
